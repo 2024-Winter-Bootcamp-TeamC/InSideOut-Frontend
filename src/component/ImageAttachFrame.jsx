b2f5e1a043242c7ec41c53bb758ccdef22792810
choi-il-woo <chldlfdn0440@naver.com>
2025-01-13 17:09:18 +0900

    Design : 이미지 첨부 프레임 만들기

--- src/component/ImageAttachFrame.jsx
+++ src/component/ImageAttachFrame.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 // PerspectiveWrapper: 카드에 원근감을 적용하는 컨테이너
 // 원근 효과를 부여하려면 부모 요소에 `perspective`를 설정해야 함
 const PerspectiveWrapper = styled.div`
-  perspective: 1000px; /* 원근감 설정 (값이 작을수록 더 극적인 효과) */
+  perspective: 600px; /* 원근감 설정 (값이 작을수록 더 극적인 효과) */
   display: flex;
   align-items: center; /* 세로 중앙 정렬 */
   justify-content: center; /* 가로 중앙 정렬 */
@@ -13,12 +13,14 @@ const PerspectiveWrapper = styled.div`
 
 // StyledCard: 기울어진 카드 자체의 스타일 정의
 const StyledCard = styled.div`
-  width: 295.65px; /* 카드 너비 */
-  height: 323.35px; /* 카드 높이 */
-  background-color: rgba(255, 255, 255, 0.5); /* 반투명 흰색 배경 */
-  border: 5px solid rgba(0, 0, 0, 0.5); /* 반투명 검은색 테두리 */
-  transform: rotateX(15deg) rotateY(35deg); /* X축과 Y축 회전으로 입체감 구현 */
-  transform-origin: center; /* 회전 기준을 카드 중앙으로 설정 */
+  width: 295.65px;
+  height: 323.35px;
+  left: 18px;
+  top: 323.53px;
+
+  background: #ffffff;
+  border: 5px solid rgba(0, 0, 0, 0.5);
+  transform: matrix(1, 0.04, 0, 1, 0, 0);
 `;
 
 // StyledCardContent: 카드 내부 콘텐츠를 감싸는 컨테이너
@@ -39,3 +41,16 @@ export default function DivWrapper() {
     </PerspectiveWrapper>
   );
 }
+
+/*box-sizing: border-box;
+
+position: absolute;
+width: 295.65px;
+height: 323.35px;
+left: 18px;
+top: 323.53px;
+
+background: #FFFFFF;
+border: 5px solid rgba(0, 0, 0, 0.5);
+transform: matrix(1, 0.04, 0, 1, 0, 0);
+*/

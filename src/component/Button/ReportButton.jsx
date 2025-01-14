5c0c4ec5d36bfb85f7bedb4a3e9a13f21560407a
choi-il-woo <chldlfdn0440@naver.com>
2025-01-14 20:12:56 +0900

    feat : 버튼 기능 추가

--- src/component/Button/ReportButton.jsx
+++ src/component/Button/ReportButton.jsx
@@ -1,22 +1,50 @@
-import Reprot from "../../assets/Report.svg";
+import Report from "../../assets/Report.svg";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
-export const ReportButton = () => {
+export const ReportButton = ({ onClick }) => {
   return (
-    <Box>
-      <img className="Reprot" alt="Reprot" src={Reprot} />
-    </Box>
+    <BoxButton
+      onClick={(e) => {
+        e.currentTarget.blur();
+        onClick();
+      }}
+      aria-label="Report"
+    >
+      <img className="icon" alt="Report" src={Report} />
+    </BoxButton>
   );
 };
 
-const Box = styled.div`
-  width: 71px;
-  height: 59px;
+ReportButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+const BoxButton = styled.button`
+  width: 60px;
+  height: auto;
   position: absolute;
   left: 74px;
   bottom: 53px;
-  fill: rgba(86, 46, 207, 0.8);
-  stroke-width: 5px;
-  stroke: #fff;
+  background: none; /* 배경 제거 */
+  border: none; /* 테두리 제거 */
+  padding: 0; /* 여백 제거 */
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  .icon {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  /* 호버 상태에서도 아무 변화 없음 */
+  &:hover .icon {
+    transform: none;
+    filter: none;
+  }
 `;
+
 export default ReportButton;

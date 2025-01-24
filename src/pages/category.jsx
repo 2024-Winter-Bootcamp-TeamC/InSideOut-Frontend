04b764f21797018c62e4696a1204328377135f92
choi-il-woo <chldlfdn0440@naver.com>
2025-01-25 03:03:01 +0900

    Design : category1 두개 있던거 하나 삭제

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -39,18 +39,6 @@ const ContentContainer = styled.div`
   box-sizing: border-box;
 `;
 
-// 카테고리 제목 스타일
-const CategoryTitle1 = styled.div`
-  position: absolute;
-  color: #fff;
-  text-align: center;
-  font-family: "IntensaFuente", sans-serif;
-  font-size: 30px;
-  line-height: 160%;
-  font-family: "BM HANNA Pro";
-  top: 27%;
-`;
-
 const CategoryContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -66,7 +54,7 @@ const CategoryTitle = styled.div`
   font-family: "BMHANNAPro", sans-serif;
   font-size: 100px;
   line-height: 160%;
-    font-family: "BM HANNA Pro";
+  font-family: "BM HANNA Pro";
 `;
 
 const CategoryTitle1 = styled.div`
